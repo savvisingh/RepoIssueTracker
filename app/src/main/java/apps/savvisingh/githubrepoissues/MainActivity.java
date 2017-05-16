c39d988cccf997307f3ee294882c9d8588c6c4aa
Sarabjeet Singh <sabbispeak@gmail.com>
2017-05-16 16:42:09 +0530

    Default Text and ProgressBar Added

--- app/src/main/java/apps/savvisingh/githubrepoissues/MainActivity.java
+++ app/src/main/java/apps/savvisingh/githubrepoissues/MainActivity.java
@@ -1,5 +1,7 @@
 package apps.savvisingh.githubrepoissues;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +9,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -41,26 +46,44 @@ public class MainActivity extends AppCompatActivity {
     private Disposable searchDisposable;
 
     private Button button;
+
+    private TextView noIssueText, noSearchText;
+
+    private ProgressBar mProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initRecyclerView();
+
 
         composite = new CompositeDisposable();
 
         editText = (EditText) findViewById(R.id.searchQuery);
         button  =(Button) findViewById(R.id.search);
+        noIssueText = (TextView) findViewById(R.id.no_issue_text);
+        noSearchText = (TextView) findViewById(R.id.no_search_Query);
+        mProgressDialog = (ProgressBar) findViewById(R.id.progressbar_loading);
+
+        initRecyclerView();
 
         retrofit = ApiClient.getClient();
 
         final ApiInterface GitHubAPI = retrofit.create(ApiInterface.class);
 
         button.setOnClickListener(view -> {
+
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
             String s = editText.getText().toString();
             String[] ss = s.split("/");
 
+
+            mRecyclerView.setVisibility(View.INVISIBLE);
+            noSearchText.setVisibility(View.INVISIBLE);
+            noIssueText.setVisibility(View.INVISIBLE);
+            mProgressDialog.setVisibility(View.VISIBLE);
 
             if(searchDisposable!=null){
                 searchDisposable.dispose();
@@ -80,12 +103,25 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void loadData(List<Issue> issues){
+
+
+
+        mProgressDialog.setVisibility(View.INVISIBLE);
+
+        if(issues.size()==0){
+            noIssueText.setVisibility(View.VISIBLE);
+        }else {
+            mRecyclerView.setVisibility(View.VISIBLE);
             mIssuesList.clear();
             mIssuesList.addAll(issues);
             mAdapter.notifyDataSetChanged();
+        }
+
     }
 
     public void onError(Throwable e) {
+        mProgressDialog.setVisibility(View.INVISIBLE);
+        noIssueText.setVisibility(View.VISIBLE);
         Log.d("Error", " " + e.getLocalizedMessage());
         Snackbar snackbar = Snackbar
                 .make(findViewById(R.id.coordinatorLayout), "No such User and Repo", Snackbar.LENGTH_LONG);
@@ -93,6 +129,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initRecyclerView() {
+
+
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -102,6 +140,12 @@ public class MainActivity extends AppCompatActivity {
         mIssuesList = new ArrayList<>();
         mAdapter = new DataAdapter(mIssuesList);
         mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        noIssueText.setVisibility(View.INVISIBLE);
+        noSearchText.setVisibility(View.VISIBLE);
+        //mProgressDialog.setVisibility(View.INVISIBLE);
+
 
     }
 
